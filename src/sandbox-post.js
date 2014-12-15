8874424477e7e5dea38684defea1ea59f9e58bfd
Gejgalis, Piotr <Piotr.Gejgalis@sabre.com>
2014-12-15 14:43:24 +0100

    added CommonJS entry in UMD wrapper

--- src/sandbox-post.js
+++ src/sandbox-post.js
@@ -3,6 +3,8 @@
 
     if (typeof define === "function" && typeof define.amd === "object" && define.amd.vendor !== "dojotoolkit.org") {
         define(["jquery", "izi-js"], amdFactory);
+    } else if (typeof exports === 'object') {
+        module.exports = amdFactory(require("jquery"), require("izi-js"));
     } else {
         global.izi = amdFactory(global.$, global.izi);
     }
