47b5c7bb3e578172012f954e3bd9d41372c3ae85
Gejgalis, Piotr <piotr@gejgalis.com>
2014-12-15 15:21:03 +0100

    dist

--- dist/izi-js-jquery.js
+++ dist/izi-js-jquery.js
@@ -457,6 +457,8 @@ $.iziDelegate = function (event, subSelector) {
 
     if (typeof define === "function" && typeof define.amd === "object" && define.amd.vendor !== "dojotoolkit.org") {
         define(["jquery", "izi-js"], amdFactory);
+    } else if (typeof exports === 'object') {
+        module.exports = amdFactory(require("jquery"), require("izi-js"));
     } else {
         global.izi = amdFactory(global.$, global.izi);
     }
